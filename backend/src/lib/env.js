2ea9e5c5990650e278aab6ab3051ddbbf21a017a
jayt-21 <jayantthakur405@gmail.com>
2026-02-19 12:46:59 +0530

    deployment config added

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,6 +5,7 @@ dotenv.config();
 export const ENV={
     PORT:process.env.PORT,
     DB_URL:process.env.DB_URL,
+    NODE_ENV:process.env.NODE_ENV,
 }
 
 export default ENV;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,26 @@
 
 import express from 'express';
 import {ENV} from './lib/env.js';
+import path from 'path';
 
 const app=express();
 const PORT = ENV.PORT||3000;
 const DB_URL=ENV.DB_URL;
-app.get('/health',(req,res)=>{
-    res.status(200).json({msg:"api running 7"});
+
+const __dirname = path.resolve();
+
+app.get('/books',(req,res)=>{
+    res.status(200).json({msg:"this is the books endpoint"});
 });
+
+//make app ready for deployment
+if(ENV.NODE_ENV==='production')
+{
+    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+    app.get('/{*any}',(req,res)=>{
+        res.sendFile(path.join(__dirname,'../frontend/dist/index.html'));
+    })
+}
 
 app.listen(PORT,()=>{
     console.log('Server is running on port '+PORT);
